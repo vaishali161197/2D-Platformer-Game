fce74fa1de4c861302c27278bcd80796d3293b9a
vaishali161197 <vaishali161197vs@gmail.com>
2020-10-04 16:48:21 +0530

    chapter-5

---
+++ Assets/Scripts/GameOver.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+    public string newscene;
+    // Start is called before the first frame update
+    //void Start()
+    //{
+
+    //}
+
+    // Update is called once per frame
+    //void Update()
+    //{
+
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            Debug.Log(" GAMEOVER");
+            SceneManager.LoadScene(newscene);
+        }
+
+    }
+}
